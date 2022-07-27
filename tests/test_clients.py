d62135110d844ffc13647513b25d307747bae439
Daniel Seifert <info@danielseifert.ch>
2022-07-27 09:15:56 +0200

    chore(PYL-E0102): rename redefined test functions

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -130,7 +130,7 @@ async def test_async_client_non_async_backend(strawberry_backend):
 
 
 @pytest.mark.asyncio
-async def test_async_client_query(strawberry_backend):
+async def test_async_client_query_non_async_backend(strawberry_backend):
     async with AsyncClient(strawberry_backend) as client:
         result = await client.query.getBooks(_fields=["title", "author"])
         assert isinstance(result, GraphQLResponse)
@@ -138,7 +138,7 @@ async def test_async_client_query(strawberry_backend):
 
 
 @pytest.mark.asyncio
-async def test_async_client_mutation(strawberry_backend):
+async def test_async_client_mutation_non_async_backend(strawberry_backend):
     async with AsyncClient(strawberry_backend) as client:
         result = await client.mutation.addBook(
             _fields=["title", "author"], title="1984", author="George Orwell"
